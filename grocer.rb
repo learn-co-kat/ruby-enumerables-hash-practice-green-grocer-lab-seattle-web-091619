8a1dffc7c589b2d4610bf6f0c89f09ce60c5db96
Katrina R <katrina.reinsdorff@gmail.com>
2019-09-04 16:40:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,28 @@
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+  cart.each do |item|
+    key = item.keys[0]
+    if new_hash[key]
+      new_hash[key][:count] += 1
+    else
+      nested_hash = item[key]
+      nested_hash[:count] = 1
+      new_hash[key] = nested_hash
+    end
+  end
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  item_on_sale = coupons[0][:item]
+  cart["#{item_on_sale} W/COUPON"] = {}
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  
 end
 
 def checkout(cart, coupons)
-  # code here
+  
 end
